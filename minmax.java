f122ad5ee8486c1b6ed32628d101933815aee853
Partha Das <pd6434938@gmail.com>
2025-10-15 02:35:20 +0530

    Update minmax.java

---
+++ minmax.java
@@ -0,0 +1,21 @@
+public class minmax {
+     
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 9, 1, 7};
+
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+}
