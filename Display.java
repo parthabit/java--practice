dc1119376d310a20eb46530dcc0b0165434ddd82
Partha Das <pd6434938@gmail.com>
2026-02-19 23:07:01 +0530

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,7 @@
+public class Display{
+    public static void main(String[] args) {
+        String firstname ="Partha";
+        String lastname = "Das";
+        System.out.println(firstname + lastname);
+    }
+}
