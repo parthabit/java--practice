e9d7227f103348b93212adb1e43629eebdd4cb2e
Partha Das <pd6434938@gmail.com>
2026-08-26 19:05:27 +0530

    Remove unnecessary whitespace in JokeGenerator.java

--- JokeGenerator.java
+++ JokeGenerator.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-
 public class JokeGenerator {
     private static List<String> jokes = new ArrayList<>();
     private static Random random = new Random();
